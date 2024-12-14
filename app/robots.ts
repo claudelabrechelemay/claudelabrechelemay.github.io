6beaa1ce1dfd9590035d042745a514931092f985
Shawn Inder <shawninder@gmail.com>
2024-12-13 19:49:40 -0500

    UserAgent: *, Disallow images

--- app/robots.ts
+++ app/robots.ts
@@ -15,12 +15,26 @@ const disallowedUAs = [
   'NerdyBot'
 ]
 
-export default function robots (): MetadataRoute.Robots {
+const disallowedPatterns = [
+  '/*.json$',
+  '/*_buildManifest.js$',
+  '/*_middlewareManifest.js$',
+  '/*_ssgManifest.js$',
+  '/*.js$',
+  '/_next/static/media/*',
+  '/_next/static/chunks/media/*',
+  '/_next/static/chunks/images/_next/static/media/*'
+]
+
+export default async function robots (): Promise<MetadataRoute.Robots> {
   return {
     rules: disallowedUAs.map((userAgent) => ({
       userAgent,
       disallow: '/'
-    })),
+    })).concat(disallowedPatterns.map((pattern) => ({
+      userAgent: '*',
+      disallow: pattern
+    }))),
     sitemap: 'https://www.claudelabrechelemay.com/sitemap.xml'
   }
 }
