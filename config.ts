27a7145d6e9058abdade78d038435016530a7fd9
Shawn Inder <shawninder@gmail.com>
2024-12-19 13:48:27 -0500

    Added licence information

--- config.ts
+++ config.ts
@@ -1,5 +1,8 @@
-export type Locale = 'en' | 'fr'
-export type Locales = ['en', 'fr']
-export const locales: Locales = ['en', 'fr']
-export const carouselHeight = '[--carousel-height:60svh]'
-export const carouselItemClass = 'basis-auto md:h-[calc(100svh-4.5rem)] short:h-[--carousel-height]'
+export enum Locales {
+  EN = 'en',
+  FR = 'fr'
+}
+export const locales = [Locales.EN, Locales.FR]
+export const carouselHeight = '[--carousel-height:65svh]'
+const className = 'basis-auto md:h-[calc(100svh-4.5rem)] short:h-[--carousel-height]'
+export const carouselItemClass = className
