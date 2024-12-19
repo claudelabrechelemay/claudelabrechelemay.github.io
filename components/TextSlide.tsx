8b1f3dc0a7e2826c587dc0dbede962119409173e
Shawn Inder <shawninder@gmail.com>
2024-12-19 13:47:34 -0500

    Add min-width to TextSlide

--- components/TextSlide.tsx
+++ components/TextSlide.tsx
@@ -6,7 +6,7 @@ import TextBlock from '@/components/TextBlock'
 
 export default function Text ({children}: PropsWithChildren) {
   return (
-    <CarouselItem className={cn(carouselItemClass, `flex max-w-full flex-col overflow-y-scroll p-4`)}>
+    <CarouselItem className={cn(carouselItemClass, `flex max-w-full min-w-96 flex-col overflow-y-scroll p-4`)}>
       <TextBlock>{children}</TextBlock>
     </CarouselItem>
   )
