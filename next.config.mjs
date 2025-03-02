18bcc397c3e50bc3f1cfb9ebbe68884be258df4a
Shawn Inder <shawninder@gmail.com>
2025-03-01 21:17:49 -0500

    Add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   output: 'export',
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
   // Move to Next.js 15, then:
   // experimental: {
   //   reactCompiler: true,
