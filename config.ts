917fb3ce690e49a2ca3b3b8bc4d8e5b93a1d4296
Shawn Inder <shawninder@gmail.com>
2025-06-16 22:39:01 -0400

    New carousel height

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@ export enum Locales {
   FR = 'fr'
 }
 export const locales = [Locales.EN, Locales.FR]
-export const carouselHeight = '[--carousel-height:65svh]'
+export const carouselHeight = '[--carousel-height:calc(300px-4rem+50svh)]'
 const className = 'basis-auto md:h-[calc(100svh-4.5rem)] short:h-(--carousel-height)'
 export const carouselItemClass = className
